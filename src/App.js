9ff90d6459b4797b7a9ce7c1f47c102bc8798614
Gabriel Coelho Cannes <gabrielcannes.gc@gmail.com>
2021-05-30 12:59:02 -0300

    aaaa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { LeitorDeArquivo } from './Components/LeitorDeArquivo/LeitorDeArquivo.jsx'
+
+import 'fontsource-roboto';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return (
+        
+<div className="Header">
+  <div className="Header-background"></div>
+  <div className="Header-content">
+    <div className="Header-hero">
+      <h1>Validador de Arquivos <br/> de Remessa</h1>
+      <LeitorDeArquivo></LeitorDeArquivo>
+      <p>Simples e fácil de usar.</p>
     </div>
-  );
+    <div className="Header-visuals">
+    </div>
+  </div>
+</div>
+    );
 }
 
 export default App;
